acecf2598d36e49fd7b12247a3dbb7250dda2c98
animesh-UCL <animesh.srivastava.23@ucl.ac.uk>
2024-03-13 12:47:47 +0000

    feat: better graphs q2

--- Topic_02/Coursework_02/Code/q2_c.m
+++ Topic_02/Coursework_02/Code/q2_c.m
@@ -35,39 +35,64 @@ if ~exist(directory, 'dir')
 end
 
 % Plot optimisation times
+% If DrivebotSLAMSystem_obj.setRecommendOptimizationPeriod() not used then
+% this will be a blank graph
 minislam.graphics.FigureManager.getFigure('Optimization times');
 clf
 plot(results{1}.optimizationTimes, '*')
-saveas(gcf, fullfile(directory, 'Optimisation_times.png'), 'png');
 hold on
+title('Optimization times')
+xlabel('Timestep')
+ylabel('Optimisation Time (sec)')
+saveas(gcf, fullfile(directory, 'Optimisation_times.png'), 'png');
 
 % Plot the error curves
 minislam.graphics.FigureManager.getFigure('Errors');
 clf
-plot(results{1}.vehicleStateHistory'-results{1}.vehicleStateHistory')
-saveas(gcf, fullfile(directory, 'Errors.png'), 'png');
+% wrap theta in [-pi, pi]
+errors = results{1}.vehicleStateHistory'-results{1}.vehicleTrueStateHistory';
+errors(:,3) = g2o.stuff.normalize_thetas(errors(:,3));
+plot(errors)
 hold on
+legend('x error', 'y error', '\psi error')
+legend('Location', 'best');
+title('Errors')
+xlabel('Timestep')
+ylabel('error')
+saveas(gcf, fullfile(directory, 'errors.png'), 'png');
 
 % Plot covariance
 minislam.graphics.FigureManager.getFigure('Vehicle Covariances');
 clf
 plot(results{1}.vehicleCovarianceHistory')
+hold on
+legend('cov(x)', 'cov(y)', 'cov(\psi)')
+legend('Location', 'best');
+xlabel('Timestep')
+title('Vehicle Covariances')
+ylabel('covariance')
 saveas(gcf, fullfile(directory, 'Vehicle_covariances.png'), 'png');
-hold on
 
-% Plot errors
-minislam.graphics.FigureManager.getFigure('Errors');
-clf
-plot(results{1}.vehicleStateHistory'-results{1}.vehicleTrueStateHistory')
-saveas(gcf, fullfile(directory, 'Errors_2.png'), 'png');
-hold on
 
 % Plot chi2 values
-minislam.graphics.FigureManager.getFigure('chi2 values');
-clf
-plot(results{1}.chi2Time, results{1}.chi2History)
-saveas(gcf, fullfile(directory, 'Chi2.png'), 'png');
-hold on
+for log_chi= [true, false]
+    minislam.graphics.FigureManager.getFigure('chi2 values');
+    clf
+    
+    if log_chi
+        plot(results{1}.chi2Time, log(results{1}.chi2History)) % notice the log
+        title('log(Chi2) values')
+        ylabel('log(Chi2) Value')
+        xlabel('Timestep')
+        saveas(gcf, fullfile(directory, 'log_Chi2.png'), 'png');
+    else
+        plot(results{1}.chi2Time, results{1}.chi2History) % notice the log
+        title('Chi2 values')
+        ylabel('Chi2 Value')
+        xlabel('Timestep')
+        saveas(gcf, fullfile(directory, 'Chi2.png'), 'png');
+    end
+end
 
 
 % This is how to extract the graph from the optimizer
